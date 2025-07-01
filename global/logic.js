e2cb275f1fb5938009d74bb94c3e5c111c27ed94
Marco88-cmd <iamthatmarco56@gmail.com>
2025-06-30 20:50:40 -0600

    Auto commit

--- global/logic.js
+++ global/logic.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const inputs = document.querySelectorAll(".unit-input");
+
+    inputs.forEach((input) => {
+        input.addEventListener("input", (event) => {
+            const value = event.target.value;
+            const unit = input.closest(".unit-card").dataset.unit;
+            console.log(`Input changed: ${value} for unit: ${unit}`);
+        });
+
+    })
 });
+
+
+function convert(value, fromUnit, toUnit) {
+    const valueInMeter = value * conversionTable[fromUnit];
+}
--- length/conversion.js
+++ length/conversion.js
@@ -1,1 +1,12 @@
-const selectedUnits = ['m', 'ft', 'km', 'in'];
+const selectedUnits = ["m", "mi", "km", "in", "cm", "mm", "um", "nm" ];
+
+const conversionTable = {
+    nm: 0.000000001,
+    um: 0.000001,
+    mm: 0.001,
+    cm: 0.01,
+    in: 0.0254,
+    m: 1,
+    km: 1000,
+    mi: 1609.34
+}
